bedbe72cf85931673eae83e5fe6e1aa88d63869b
Artsiom Trubchyk <aglitchman@gmail.com>
2022-10-06 20:36:47 +0300

    Update

--- fix.js
+++ fix.js
@@ -1,6 +1,6 @@
 
 		Progress.calculateProgress = function (from, to, current, total) {
-			console.log("progress: ", "from=", from, "to=", to, (from > to ? "<!> (from > to!)" : ""), "current=", current, "total=", total, "percentage=", (from + (current / total) * (to - from)) + "%");
+			console.log("progress: ", "from=", from, "to=", to, "current=", current, "total=", total, (current > total ? "<!!!> (current > total)" : ""), "percentage=", (from + (current / total) * (to - from)) + "%");
 
 			// THE FIX:
 			current = Math.min(total, current);
